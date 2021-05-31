3cf3c004167d0bba8c752e0642ebba7db317e0e5
favourite-debugger <69878286+favourite-debugger@users.noreply.github.com>
2021-06-01 00:02:16 +0200

    Updated this. Included camera and shooter changes

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ import * as dat from '../js/dat.gui.module.js';
 		function init(){
 			scene = new THREE.Scene();
 
-			camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1, 1000);
+			camera = new THREE.PerspectiveCamera(65, window.innerWidth/window.innerHeight,0.1, 1000);
 
 			renderer = new THREE.WebGLRenderer({antialias:true});
 			renderer.setSize(window.innerWidth, window.innerHeight);
