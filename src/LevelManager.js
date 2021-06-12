4e6d21a350f053c42936b40cb6dca2eae9bf03ba
exit <naickereston@gmail.com>
2021-06-12 18:58:32 +0200

    home ui Done

--- src/LevelManager.js
+++ src/LevelManager.js
@@ -1,9 +1,19 @@
 import {Level1} from './level1.js'
 // export {Manager}
-var LV1 = document.getElementById("Level1")
+var LV1 = document.getElementById("Level1");
+var LV2 = document.getElementById("Level2");
+var LV3 = document.getElementById("Level3");
 
 LV1.onclick = function(){
-  document.body.innerHTML = "";
-   LV1.style.display = "none"
+  document.getElementById('div').style.display = 'none';
   const lv1 = new Level1();
 }
+//for later 
+// LV2.onclick = function(){
+//   document.getElementById('div').style.display = 'none';
+//   const lv2 = new Level2();
+// }
+// LV3.onclick = function(){
+//   document.getElementById('div').style.display = 'none';
+//   const lv3 = new Level3();
+// }
