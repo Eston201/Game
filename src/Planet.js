63edbe1f337bc3539a91fa3ab8345de00771ae3f
favourite-debugger <69878286+favourite-debugger@users.noreply.github.com>
2021-06-14 22:49:04 +0200

    added updated planets and other changes

--- src/Planet.js
+++ src/Planet.js
@@ -1,44 +1,75 @@
 import * as THREE from '../js/three.module.js';
-export {planet}
+export {Planet}
 import * as dat from '../js/dat.gui.module.js'
 
-class planet {
+class Planet {
 
-  constructor(params) {
-    this.createPlanet(params);
+  constructor(params,pval) {
+    this.createPlanet(params,pval);
   }
 
-  createPlanet(params){
+  createPlanet(params, pval){
+    this.params = params;
     this.planet = new THREE.Object3D();
 
-    this.pgeo = new THREE.SphereGeometry(100,50,50);
-    this.pmat = new THREE.MeshLambertMaterial({color : 0x2B7BEA});
-    this.planetobj = new THREE.Mesh(this.pgeo,this.pmat);
+    const earthtext = new THREE.TextureLoader().load(
+      'resources/textures/earth.jpeg'
+    );
+  const marstext = new THREE.TextureLoader().load(
+      'resources/textures/spacerock.jpg'
+    );
+  const magmatext = new THREE.TextureLoader().load(
+      'resources/textures/magma.jpg'
+    );
+  const bluetext = new THREE.TextureLoader().load(
+      'resources/textures/blue.jpg'
+    );
+  const tarr = [earthtext,
+      marstext,
+      magmatext,
+      bluetext
+  ];
 
-    this.bgeo = new THREE.SphereGeometry(3,32,32);
-    this.bmat = new THREE.MeshPhongMaterial({color : 0xD1AE87});
-    this.beltobj = new THREE.Mesh(this.bgeo,this.bmat);
-    this.beltobj.position.x = 102;
+  const pgeo = new THREE.SphereGeometry(150,80,80);
+  const pmat = new THREE.MeshBasicMaterial({map : tarr[pval]});
+  const planetobj = new THREE.Mesh(pgeo,pmat);
 
-    for (let i=0;i<30;i++){
-        this.belt = new THREE.Mesh(this.bgeo,this.bmat);
-        this.belt.position.x = 102*Math.cos(i*0.209);
-        this.belt.position.z = 102*Math.sin(i*0.209);
-        this.belt.position.y = 50*Math.sin(i*0.209);
-        this.planet.add(this.belt);
+    this.planet.add(planetobj);
+    this.params.scene.add(this.planet);
+
+  }
+
+  addBelt(){
+    const gravplanet = new THREE.Object3D();
+
+    const asttext = new THREE.TextureLoader().load(
+        'resources/textures/spacerock.jpg'
+    );
+    
+    const bgeo = new THREE.SphereGeometry(3,32,32);
+    const bmat = new THREE.MeshLambertMaterial({map : asttext});
+    
+
+    const rotval = (1/50)*2*Math.PI;
+    for (let i=0;i<50;i++){
+        const belt = new THREE.Mesh(bgeo,bmat);
+         belt.position.x = 162*Math.cos(i*rotval);
+         belt.position.z = 162*Math.sin(i*rotval);
+         belt.position.y = 30*Math.sin(i*rotval);
+         gravplanet.add(belt);
 
     }
 
-    this.planet.add(this.beltobj);
-
-    this.planet.add(this.planetobj);
-
-
+    gravplanet.position.copy(this.planet.position);
+    this.planet.add(gravplanet);
+    this.params.scene.add(gravplanet);
   }
+
   animate(){
     this.planet.rotation.z += 0.00002;
     this.planet.rotation.x += 0.00002;
     this.planet.rotation.y += 0.02;
+
   }
 
 };
